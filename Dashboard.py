74d44b21e6f5008a186bda5a701ead4fea8f0fa9
sebmatecho <slozanof@unal.edu.co>
2023-03-18 19:43:59 -0500

    debuggin figure

--- Dashboard.py
+++ Dashboard.py
@@ -403,15 +403,14 @@ def load(data,geo_data):
      # for _ , fila in data.iterrows():
      for i in [1,2]:
           folium.Marker([data.loc[i,'lat'],data.loc[i,'long']],#[fila['lat'],fila['long']],
-                         popup = f'this is a test {i}'
-                         # 'Price: ${}, \n Date: {} \n {} # rooms \n {} # bathrooms \n Built in {} \n  {} square foot \n Price per square foot: {}'.format(
-                         # fila['price'],
-                         # fila['date'],
-                         # fila['bedrooms'],
-                         # fila['bathrooms'],
-                         # fila['yr_built'], 
-                         # fila['sqft_living'], 
-                         # fila['price/sqft'])
+                         popup = 'Price: ${}, \n Date: {} \n {} # rooms \n {} # bathrooms \n Built in {} \n {} square foot \n Price per square foot: {}'.format(
+                         data.loc[i,'price'],
+                         data.loc[i,'date'],
+                         data.loc[i,'bedrooms'],
+                         data.loc[i,'bathrooms'],
+                         data.loc[i,'yr_built'], 
+                         data.loc[i,'sqft_living'], 
+                         data.loc[i,'price/sqft'])
           ).add_to(markercluster)
      folium_static(mapa, width=width, height=0.33*width)
 
